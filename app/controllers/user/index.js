c55650a7f634b68c15158f458a2cd5e76056e12e
David Collette <collettedj@gmail.com>
2016-02-18 22:18:41 -0800

    add dialog to create a client application

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -2,9 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     showNewApp:false,
+
+    newClient: Ember.computed({
+        get(){
+            return this.store.createRecord('client');
+        }
+    }),
+
     actions:{
         toggleNewApp(){
             this.toggleProperty('showNewApp');
+        },
+
+        saveNewClient(){
+            const newClient = this.get('newClient');
+            newClient.save()
+                .catch(err => {
+                    console.log(err);
+                });
         }
     }
 });
---
+++ tests/integration/components/edit-client-app-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('edit-client-app', 'Integration | Component | edit client app', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+
+  this.render(hbs`{{edit-client-app}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:" + EOL +
+  this.render(hbs`
+    {{#edit-client-app}}
+      template block text
+    {{/edit-client-app}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
