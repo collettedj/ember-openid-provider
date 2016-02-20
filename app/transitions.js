2ef52cf259af064a468deb3050c96d9c89211199
David Collette <collettedj@gmail.com>
2016-02-19 21:15:32 -0800

    some minor UI changes to make the transition from user to user client look a little nicer

--- app/transitions.js
+++ app/transitions.js
@@ -2,7 +2,7 @@ export default function(){
   this.transition(
     this.fromRoute('user.index'),
     this.toRoute('user.client'),
-    this.use('toUp'),
-    this.reverse('toDown')
+    this.use('toLeft'),
+    this.reverse('toRight')
   );
 }
